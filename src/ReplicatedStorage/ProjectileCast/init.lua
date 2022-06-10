f43e8c6d0d1629823aa20260d7d16baee23f11fd
Ryan Chang <ryanchang12321@gmail.com>
2022-06-10 16:32:45 -0400

    Update init.lua

--- src/ReplicatedStorage/ProjectileCast/init.lua
+++ src/ReplicatedStorage/ProjectileCast/init.lua
@@ -1,3 +1,13 @@
--- Original Module can be found at - 
--- API Documentation can be found at -
+--[[
+
+	Coded and written by Ryanisawesome25 (21932606)
+	
+	API Documentation can be found at - https://github.com/RyanChang25/Projectile-Cast-v0.4.1
+	
+	Original Module - https://www.roblox.com/library/9866876932/Projectile-Cast
+	
+	** If anything is broken, please don't hesitate to message me! **
+
+--]]
+
 return {Client = require(script.Client), Server = require(script.Server), TargetSettings = require(script.TargetSettings)}
