57f01d509aa360ee5556ff401e765619ec0a4d91
Ryan Chang <ryanchang12321@gmail.com>
2022-06-16 00:06:30 -0400

    Update init.lua

--- src/ReplicatedStorage/ProjectileCast/init.lua
+++ src/ReplicatedStorage/ProjectileCast/init.lua
@@ -1,14 +1,13 @@
 --[[
     
-	-->>: PLACE THIS MODULE INTO REPLICATED STORAGE
+	-->>: IMPORTANT: PLACE THIS MODULE INTO REPLICATED STORAGE!
+	
+	====================================================================
 	
 	Coded and developed by Ryanisawesome25 (21932606)
-	
 	API Documentation can be found at - https://github.com/RyanChang25/Projectile-Cast
-	
 	Original Module - https://www.roblox.com/library/9866876932/Projectile-Cast
-	
-	** If anything is broken or you have any questions, please don't hesitate to message me on Twitter or Discord! **
+	** If anything is broken or you have any questions, please don't hesitate to message me on Roblox, Twitter or Discord! **
 
 --]]
 
